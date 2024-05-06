17f4aa000899104b6f31f105bd8d8f5098789304
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-05-06 12:31:44 +0200

    fix: [MCIL-105] prevent failure if credentials are not set

--- src/util.py
+++ src/util.py
@@ -5,7 +5,7 @@ import os
 
 def load_credentials():
     credentials_base64 = os.environ.get('CLIENT_CREDENTIALS_BASE64')
-    if credentials_base64 is None:
+    if credentials_base64 is None or credentials_base64 == '':
         return json.loads('{}')
     client_credentials = base64.b64decode(credentials_base64).decode('utf-8')
     return json.loads(client_credentials)
